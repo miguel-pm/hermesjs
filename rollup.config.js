0bc73b95b821342d9e381416026a77a0153ef735
MiguelPM <miguelpm@protonmail.com>
2020-02-09 17:45:56 +0100

    feat: add build scripts and files

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+import babel from 'rollup-plugin-babel'
+import dts from 'rollup-plugin-dts'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+
+export default [
+  {
+    input: './src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()]
+  },
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        format: 'cjs',
+        file: './dist/index.js'
+      },
+      {
+        format: 'es',
+        file: './dist/index.es.js'
+      }
+    ],
+    external: ['mongoose'],
+    plugins: [
+      resolve({ extensions: ['.js', '.ts'], preferBuiltins: true }),
+      commonjs(),
+      babel({
+        runtimeHelpers: true,
+        extensions: ['.js', '.ts'],
+        include: ['src/**/*']
+      })
+    ]
+  }
+]
