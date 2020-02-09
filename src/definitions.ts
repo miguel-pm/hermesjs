703c8e8b86f3e8fe8272752172ea4095b26a314a
MiguelPM <miguelpm@protonmail.com>
2020-02-09 18:00:36 +0100

    feat: Add Hermes functionality

---
+++ src/definitions.ts
@@ -0,0 +1,59 @@
+import {
+  TemplatedApp,
+  HttpResponse,
+  HttpRequest,
+  RecognizedString
+} from 'uWebSockets.js'
+import { Logger } from 'winston'
+
+export enum RequestMethods {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  DELETE = 'DELETE'
+}
+
+export interface MainDependencies {
+  logger: Logger | Console
+}
+
+export interface GenAppErrorFunction {
+  (status: RecognizedString, message: string, error?: Error): HermesError
+}
+
+export interface BootstrapFunction {
+  (deps: MainDependencies, router: RouterFunction, app?: TemplatedApp, port?: number): void
+}
+export interface RequestHandlerFunction {
+  (deps: MainDependencies, router: RouterFunction, res: HttpResponse, req: HttpRequest): Promise<HttpResponse>
+}
+export interface BoundRequestHandler {
+  (res: HttpResponse, req: HttpRequest): Promise<HttpResponse>
+}
+export interface RouterFunction {
+  (deps: MainDependencies, req: RequestData, res: HttpResponse): Promise<HttpResponse>
+}
+export interface ErrorHandler {
+  (res: HttpResponse, logger: Logger | Console, error: HermesError): HttpResponse
+}
+export interface BoundErrorHandler {
+  (error: HermesError)
+}
+
+export interface RequestBody {
+  [key: string]: string | number
+}
+
+export interface RequestData {
+  method: RequestMethods
+  route: string
+  queryParams: { [key: string]: string }
+  body: RequestBody | {}
+  headers: Map<string, string>
+}
+
+export interface HermesError {
+  status?: RecognizedString
+  message?: string
+  error?: Error
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,55 @@
-console.log('hello world')
+import { App } from 'uWebSockets.js'
+
+import { requestHandler, errorHandler } from './utils'
+import {
+  HermesError,
+  BootstrapFunction,
+  BoundRequestHandler,
+  BoundErrorHandler
+} from './definitions'
+
+import { DEFAULT_PORT } from './constants/server.json'
+import { SERVER_LISTEN_ERROR_MESSAGE } from './constants/messages.json'
+
+/**
+ * @function
+ * @description Main Bootstrap function of the Hermes.js framework. Expects a set of dependencies
+ * (a logging object is required but it can be Nodejs Console object) and a router function to handle routes and executions.
+ * A specific uWebSockets TemplatedApp object and port number might be specified.
+ * @param deps - Set of injected dependencies. Only mandatory one is a logging object (might be Node's console object)
+ * @param router - A user defined function to handle routes and logic
+ * @param [app] - (Optional) uWebSockets TemplatedApp object
+ * @param [port] - (Optional) Port number to have the server connect at
+ */
+const Hermes: BootstrapFunction = (deps, router, app = App(), port = DEFAULT_PORT) => {
+  const { logger } = deps
+  const reqHandler: BoundRequestHandler = requestHandler.bind(null, deps, router)
+  app
+    .any('/*', (res, req) => {
+      const errHandler: BoundErrorHandler = errorHandler.bind(null, res, logger)
+      return reqHandler(res, req)
+        .catch((error: Error | HermesError) => {
+          if (error instanceof Error) error = { error } as HermesError
+          return errHandler(error)
+        })
+    })
+    .listen(port, listenSocket => {
+      if (listenSocket) logger.info(`Listening on port ${port}`)
+      else throw new Error(SERVER_LISTEN_ERROR_MESSAGE)
+    })
+}
+
+export { genAppError } from './utils'
+
+export {
+  RouterFunction,
+  RequestMethods,
+  MainDependencies
+} from './definitions'
+
+export {
+  OK_STATUS,
+  UNPROCESSABLE_ENTITY_STATUS
+} from './constants/http_status.json'
+
+export default Hermes
