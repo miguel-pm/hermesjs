9c459c288c9da0e12905a04ecfc0d94c8fc1a545
MiguelPM <miguelpm@protonmail.com>
2020-08-20 22:56:29 +0200

    fix: export more interfaces

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import * as hermesTypes from './hermes.types';
 
 export default hermes;
 
+export type HermesFunction = hermesTypes.HermesFunction;
 export type RouterFunction = hermesTypes.RouterFunction;
 
 export type RequestBody = hermesTypes.RequestBody;
+export type RequestData = hermesTypes.RequestData;
 export type RequestMethods = hermesTypes.RequestMethods;
 export type RequestResponse = hermesTypes.RequestResponse;
